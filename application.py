04510598ad68754c01bb937e33509c8022bf1c1b
lauragreemko <laura.barreda@greemko.com>
2022-10-13 18:49:26 +0200

    added files

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import psycopg2
 
 application = Flask(__name__)
 
@@ -6,8 +7,30 @@ application = Flask(__name__)
 def hello_world():
     return 'Desafio API test'
 
-@application.route('/result', methods=['GET'])
+@application.route('/category', methods=['GET'])
 def result():
-    brand = request.args.get('brand', 'me')
-    result = 'The brand is ' + brand
-    return result
+    category = request.args.get('category', None)
+
+    db = psycopg2.connect(host='desafio-tripulaciones.c5mpkjhryqaz.us-east-2.rds.amazonaws.com',
+                            port=5432,
+                            user='postgres',
+                            password='desafiotripulaciones',
+                            database='etiqueta_energetica')
+
+    db.autocommit=True
+
+    cursor = db.cursor()
+
+    select_query = 'SELECT "Brand", "Model" FROM ' + category
+
+    cursor.execute(select_query)
+
+    records = cursor.fetchall()
+
+    list_brand = [elem[0] for elem in records]
+    list_model = [elem[1] for elem in records]
+
+    dict = {'Brand': list_brand,
+            'Model' : list_model}
+
+    return jsonify(dict)

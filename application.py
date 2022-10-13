b4a34389afcf31df8214e34a8ba711e3f2a0014f
lauragreemko <laura.barreda@greemko.com>
2022-10-13 16:31:13 +0200

    added result

--- application.py
+++ application.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
     return 'Desafio API test'
+
+@application.route('/result', methods=['GET'])
+def result():
+    brand = request.args.get('brand', None)
+    result = 'The brand is ' + brand
+    return result

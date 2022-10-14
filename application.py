9a692a8a2e3f22cc8b71f3ca2a27a0c9cdcaaa13
lauragreemko <laura.barreda@greemko.com>
2022-10-14 11:05:36 +0200

    added kwh

--- application.py
+++ application.py
@@ -1,20 +1,29 @@
 from flask import Flask, request
-from flask_cors import CORS
+# from flask_cors import CORS
+
 from classes.category import *
+from classes.kwh import *
 from functions import *
 
 application = Flask(__name__)
-CORS(application)
+# CORS(application)
 
 @application.route('/')
 def hello_world():
     return 'Desafio API test'
 
 @application.route('/category', methods=['GET'])
-def result():
+def category():
     category = get_argument('category')
     if category == None:
         return 'Missing argument'
     else:
         query = Category(category)
         return query.json
+
+@application.route('/calculate', methods=['GET'])
+def calculate():
+    scrap = KWh()
+    return scrap.price
+
+application.run()
---
+++ classes/kwh.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup as bs
+import requests
+
+class KWh:
+    url = 'https://tarifaluzhora.es/'
+    def __init__(self):
+        self.create_soup()
+        self.get_price()
+    
+    def create_soup(self):
+        response = requests.get(self.url)
+        html = response.content
+        self.soup = bs(html, "lxml")
+    
+    def get_price(self):
+        self.price = float(self.soup.find('span', class_='main_text').text[:-2])
+        self.price = str(self.price)

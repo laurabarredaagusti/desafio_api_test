819c4c46bda7461c563e2cb24ddc30b8fb7ce9d1
lauragreemko <laura.barreda@greemko.com>
2022-10-14 11:21:43 +0200

    first request

--- classes/kwh.py
+++ classes/kwh.py
@@ -5,11 +5,11 @@ class KWh:
     url = 'https://tarifaluzhora.es/'
     def __init__(self):
         pass
-        # self.create_soup()
+        self.create_soup()
         # self.get_price()
     
-    # def create_soup(self):
-    #     response = requests.get(self.url)
+    def create_soup(self):
+        response = requests.get(self.url)
     #     html = response.content
     #     self.soup = bs(html, "lxml")
     

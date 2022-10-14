2627ca535046e2bfaf475b75e2263e26da8d0105
lauragreemko <laura.barreda@greemko.com>
2022-10-14 13:44:39 +0200

    removed spaces brands

--- classes/category.py
+++ classes/category.py
@@ -39,11 +39,12 @@ class Category:
     def get_brand_list(self):
         self.brand_list = [elem[0] for elem in self.records]
         self.brand_list = list(set(self.brand_list))
+        self.brand_list = [elem[1:-1] for elem in self.brand_list]
 
     def get_equivalences(self):
         self.model_list = [elem[1] for elem in self.records]
         self.model_list = list(set(self.model_list))
-
+        self.model_list = [elem[1:-1] for elem in self.model_list]
         self.equiv_dict = {'Brand': self.brand_list,
                            'Model' : self.model_list}
 

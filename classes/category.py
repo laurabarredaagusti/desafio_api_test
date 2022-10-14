e78322580c062217a067c3320b2c1c712257e27c
lauragreemko <laura.barreda@greemko.com>
2022-10-14 14:17:50 +0200

    added json

--- classes/category.py
+++ classes/category.py
@@ -39,19 +39,18 @@ class Category:
     def get_brand_list(self):
         self.brand_list = [elem[0] for elem in self.records]
         self.brand_list = list(set(self.brand_list))
-        self.brand_list = [elem[1:-1] for elem in self.brand_list]
-        self.brand_list = [elem.lower() for elem in self.brand_list]
+        self.brand_list_mod = [elem[1:-1] for elem in self.brand_list]
+        self.brand_list_mod = [elem.lower() for elem in self.brand_list_mod]
 
     def get_equivalences(self):
-        self.model_list = [elem[1] for elem in self.records]
-        self.model_list = list(set(self.model_list))
-        self.model_list = [elem[1:-1] for elem in self.model_list]
-        self.model_list = [elem.lower() for elem in self.model_list]
-        self.equiv_dict = {'Brand': self.brand_list,
-                           'Model' : self.model_list}
+        self.equiv_dict = dict((key, []) for key in self.brand_list)
+        for elem in self.records:
+            brand = elem[0]
+            model = elem[1]
+            self.equiv_dict[brand].append(model[1:-1].lower())
 
     def get_json(self):
-        self.equiv_dict = {'Brand': self.brand_list,
+        self.equiv_dict = {'Brand': self.brand_list_mod,
                            'Model_by_brand' : self.equiv_dict}
 
         self.json = jsonify(self.equiv_dict)

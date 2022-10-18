4b319a4e83847b4d59747ba53999e4d6b3a1a541
lauragreemko <laura.barreda@greemko.com>
2022-10-18 12:56:48 +0200

    removed entorno

--- variables.py
+++ variables.py
@@ -1,15 +1,9 @@
-# host='desafio-tripulaciones.c5mpkjhryqaz.us-east-2.rds.amazonaws.com'
-# port=5432
-# user='postgres'
-# password='desafiotripulaciones'
-# database='etiqueta_energetica'
+host='desafio-tripulaciones.c5mpkjhryqaz.us-east-2.rds.amazonaws.com'
+port=5432
+user='postgres'
+password='desafiotripulaciones'
+database='etiqueta_energetica'
 
 kwh_price_path = 'files/kwh_price.json'
 
-# API_KEY = '923fc55c-57d8-4147-bf38-ef23d7e707ad'
-
-from boto.s3.connection import S3Connection
-import os
-
-
-s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
+API_KEY = '923fc55c-57d8-4147-bf38-ef23d7e707ad'
